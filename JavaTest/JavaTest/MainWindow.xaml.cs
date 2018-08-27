32e1cdaf599f21fd8ff708158bf48b9059bbe292
VineetVenugopal <vineet142008@gmail.com>
2018-08-27 10:23:53 +0530

    updated

---
+++ JavaTest/JavaTest/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Net;
+using System.IO;
+
+namespace JavaTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var request = (HttpWebRequest)WebRequest.Create("http://localhost:8080/Test/Test1");
+            //request.GetResponse();
+
+            //var response = (HttpWebResponse)request.GetResponse();
+
+            // MessageBox.Show(response.ToString());
+            btnCheck.Content = "Success";
+
+        }
+    }
+}
